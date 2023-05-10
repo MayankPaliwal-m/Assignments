38fb359a0d8af835d0dadbbd0d7b880dfd2597e8
mayank <mayankpaliwal9644@gmail.com>
2023-05-10 07:09:49 +0530

    may be i have learnt

--- Assignments/src/Person.java
+++ Assignments/src/Person.java
@@ -27,15 +27,5 @@ public class Person {
         }
     }
 
-    public static void main(String[] args) {
 
-        Person personObj1 = new Person("kaliya");
-
-        Person personObj2 = new Person("elu", 25);
-
-
-        personObj1.summaryOfPerson();
-        personObj2.summaryOfPerson();
-
-    }
 }
---
+++ Assignments/src/PersonSummary.java
@@ -0,0 +1,13 @@
+public class PersonSummary {
+    public static void main(String[] args) {
+
+        Person personObj1 = new Person("kaliya");
+
+        Person personObj2 = new Person("elu", 25);
+
+
+        personObj1.summaryOfPerson();
+        personObj2.summaryOfPerson();
+
+    }
+}
